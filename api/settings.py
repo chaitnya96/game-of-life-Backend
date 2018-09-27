3a00e482b9c7d7fb10bd23f9d52b766b1b0fe31e
chaitnya96 <chaitnyaparikh@gmail.com>
2018-09-27 16:34:36 +0530

    settings to host on heroku

--- api/settings.py
+++ api/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p62*&dna6aeimmbivv*0f19(qtx(=8ogr!%wh_zvj7r0woi5!b'
+DJANGO_SECRET_KEY = 'p62*&dna6aeimmbivv*0f19(qtx(=8ogr!%wh_zvj7r0woi5!b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
